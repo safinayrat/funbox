3727d266091e5aeb7f76acd0661be58b341b332c
safinayrat <safinayrat95@gmail.com>
2019-03-01 23:03:02 +0400

    Updates

---
+++ precache-manifest.91df205a318ccb0d5cf1c07063dbe55c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b084151f8f7d3895e35d",
+    "url": "/funbox/static/css/main.e60121f5.chunk.css"
+  },
+  {
+    "revision": "b084151f8f7d3895e35d",
+    "url": "/funbox/static/js/main.b084151f.chunk.js"
+  },
+  {
+    "revision": "0d359277c4eefdf35244",
+    "url": "/funbox/static/js/runtime~main.0d359277.js"
+  },
+  {
+    "revision": "81926cc95f45e86de9d6",
+    "url": "/funbox/static/js/2.81926cc9.chunk.js"
+  },
+  {
+    "revision": "d4a585793b25921e845b425a52aee7d4",
+    "url": "/funbox/static/media/Cat.d4a58579.png"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/funbox/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "70be393819ed405c09e28f237d82bd64",
+    "url": "/funbox/static/media/Background.70be3938.jpg"
+  },
+  {
+    "revision": "4edb44f471de587a21a3c1e108d357e4",
+    "url": "/funbox/index.html"
+  }
+];
